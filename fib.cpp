de2bda4dc16483ee56df7511f43df08677e51a95
Asif-102 <asif1zero2@gmail.com>
2023-04-16 15:53:14 +0600

    find fib naive approach

---
+++ fib.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+    if(n <= 2){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+int main() {
+    cout << fib(5) << '\n';
+    cout << fib(15) << '\n';
+    cout << fib(50) << '\n';
+    return 0;
+}
